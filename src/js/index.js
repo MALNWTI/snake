016c53f965f5269974ffccd561e66d9538bf2e43
VHolodiuk <MALNWTI1995@gmail.com>
2020-12-30 19:13:36 +0200

    full updates

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 const ground = new Image();
-ground.src = "/img/ground.png";
+ground.src = "../img/ground.png";
 
 const foodImg = new Image();
 foodImg.src = "/img/food.png";
